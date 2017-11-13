d16ea2289e0e5c4c4ffeb2747892811ce2df66f3
xinhuixu <x.xu.1102@gmail.com>
2017-11-12 20:47:41 -0500

    comm

--- Task 2/BoundedQueue.java
+++ Task 2/BoundedQueue.java
@@ -8,25 +8,30 @@ public class BoundedQueue<T> extends javafoundations.CircularArrayQueue<T>{
     private int capacity;
     
     public BoundedQueue(int cap){
- super();
- capacity = cap;
+	super(); //super DEF_CAP is 10, assumes cap >=10
+	capacity = cap;
     }
 
+    /*Returns true if the size of the queue is equal to capacity
+      @return - if the size of the queue is equal to capacity*/
     public boolean isFull(){
- return size() == capacity;
+	return size() == capacity;
     }
 
+    /*Enques the new element if queue is not at capacity
+      @param element - the new element*/
     public void enqueue(T element){
- if (!isFull()){
-     super.enqueue(element);
- } else {
-     System.out.println("Queue is full.");
+	if (!isFull()){
+	    super.enqueue(element);
+	} else {
+	    System.out.println("Queue is full.");
  }
     }
 
+    /*Return capacity + inherited toString
+      @return - the string representation of BoundedQueue*/
     public String toString(){
- return "Capacity: " + capacity + "\n" + super.toString();
-
+	return "Capacity: " + capacity + "\n" + super.toString();
     }
     
     public int getCapacity(){
@@ -34,19 +39,19 @@ public class BoundedQueue<T> extends javafoundations.CircularArrayQueue<T>{
     }
     
     public static void main(String[] args){
- BoundedQueue<Double> b = new BoundedQueue<Double>(10);
- b.enqueue(0.5);
- b.enqueue(0.1);
- b.enqueue(0.1);
- b.enqueue(0.1);
- b.enqueue(0.1);
- b.enqueue(0.1);
- b.enqueue(0.1);
- b.enqueue(0.1);
- b.enqueue(0.1);
- b.enqueue(0.1);
- b.enqueue(0.1); //should print queue is full once
+	BoundedQueue<Double> b = new BoundedQueue<Double>(10);
+	b.enqueue(0.5);
+	b.enqueue(0.1);
+	b.enqueue(0.1);
+	b.enqueue(0.1);
+	b.enqueue(0.1);
+	b.enqueue(0.1);
+	b.enqueue(0.1);
+	b.enqueue(0.1);
+	b.enqueue(0.1);
+	b.enqueue(0.1);
+	b.enqueue(0.1); //should print queue is full once
 
- System.out.println(b);
+	System.out.println(b);
     }
 }
