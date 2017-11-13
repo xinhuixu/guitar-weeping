43708c19753dc84ff3db6341bed08059f264a0ff
xinhuixu <x.xu.1102@gmail.com>
2017-11-12 19:37:45 -0500

    add jfound

---
+++ BoundedQueue.java
@@ -0,0 +1,12 @@
+/* FILENAME: BoundedQueue.java
+ * WHO: Julia McDonald, Xinhui Xu
+ * WHAT: Assignment 7 Task 2
+ * WHEN: Fall 2017 */
+import javafoundations.*;
+
+public class BoundedQueue extends javafoundations.CircularArrayQueue{
+
+
+
+
+}
